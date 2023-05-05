70db7ce550d8ca494dd7c02e9df1a7caec324218
iGuEs5 <horizonghos7@gmail.com>
2023-05-06 00:20:15 +0200

    Add project files.

---
+++ GPAToEuropeGradingSystemSOFTWARE/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace GPAToEuropeGradingSystemSOFTWARE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(116, 259);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(84, 27);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(116, 324);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(84, 27);
+            textBox3.TabIndex = 1;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(113, 233);
+            label1.Name = "label1";
+            label1.Size = new Size(185, 20);
+            label1.TabIndex = 2;
+            label1.Text = "GPA TO EUROPE GRADING";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(113, 298);
+            label2.Name = "label2";
+            label2.Size = new Size(185, 20);
+            label2.TabIndex = 3;
+            label2.Text = "EUROPE TO GPA GRADING";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 6F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(48, 259);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 24);
+            label3.TabIndex = 4;
+            label3.Text = "INPUT GPA \r\nGRADE HERE\r\n";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 6F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(37, 327);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 24);
+            label4.TabIndex = 5;
+            label4.Text = "INPUT EUROPE\r\nGRADE HERE\r\n";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ControlLight;
+            textBox2.Location = new Point(214, 259);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(120, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ControlLight;
+            textBox4.Location = new Point(214, 324);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(120, 27);
+            textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(340, 264);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 38);
+            label5.TabIndex = 8;
+            label5.Text = "RESULT \r\n\r\n";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(340, 327);
+            label6.Name = "label6";
+            label6.Size = new Size(58, 38);
+            label6.TabIndex = 9;
+            label6.Text = "RESULT \r\n\r\n";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(410, 419);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private Label label5;
+        private Label label6;
+    }
+}
